96a3f7ab9dbf6a287fdf3142afa05681e5b46ee0
Be Hai Nguyen <behai_nguyen@hotmail.com>
2023-07-05 00:22:30 +1000

    Change to PostgreSQL.
    Supports more environment variables.
    Added method def get_database_connection():
    Documented all attributes.

--- src/bh_aust_postcode/config.py
+++ src/bh_aust_postcode/config.py
@@ -18,14 +18,55 @@ class Config:
     """Set Flask configuration from .env file."""
 
     # General Config.
-    SECRET_KEY = environ.get( 'SECRET_KEY' )
-    FLASK_APP = environ.get( 'FLASK_APP' )
-    FLASK_DEBUG = strtobool( environ.get('FLASK_DEBUG') )
+    SECRET_KEY = environ.get('SECRET_KEY')
+    FLASK_APP = environ.get('FLASK_APP')
+    FLASK_DEBUG = strtobool(environ.get('FLASK_DEBUG'))
 
-    SOURCE_POSTCODE_URL = environ.get( 'SOURCE_POSTCODE_URL' )
-    SCHEMA = environ.get( 'SCHEMA' )
-    DATABASE = environ.get( 'DATABASE' )
+    #: The URL of the source postcodes in JSON format.
+    SOURCE_POSTCODE_URL = environ.get('SOURCE_POSTCODE_URL')
+    #: Whether or not to keep a copy of the downloaded postcodes.
+    KEEP_DOWNLOADED_POSTCODES = strtobool(environ.get('KEEP_DOWNLOADED_POSTCODES'))
+
+    #: PostgreSQL database creation script: create schema and table.
+    DB_CREATE_SCRIPT = environ.get('DB_CREATE_SCRIPT')
+
+    #: The PostgreSQL schema name.
+    SCHEMA_NAME = environ.get('SCHEMA_NAME')
+    #: The PostgreSQL table name.
+    POSTCODE_TABLE_NAME = environ.get('POSTCODE_TABLE_NAME')
+
+    #: PostgreSQL host name.
+    HOST = environ.get('HOST')
+    #: PostgreSQL database. This database must exist on host.
+    DATABASE = environ.get('DATABASE')
+    #: PostgreSQL user. This is the PostgreSQL user who has access to database.
+    PG_USER = environ.get('PG_USER')
+    #: PostgreSQL password. This is password to log into the PostgreSQL.
+    PASSWORD = environ.get('PASSWORD')
+    #: PostgreSQL port. The PostgreSQL host port.
+    PORT = environ.get('PORT')
 
 def get_config():
-    """Retrieve environment configuration settings."""
+    """Retrieve environment configuration settings.
+
+    :return: object.
+    :rtype: :class:`Config`.
+    """
     return Config
+
+def get_database_connection():
+    """Construct a dictionary suitable to connect to the target PostgreSQL 
+    database using psycopg2.
+
+    :return: a dictionary represents psycopg2 connection parameters.
+    :rtype: dict.
+    """
+    cfg = get_config()
+
+    return {
+        'host': cfg.HOST,
+        'database': cfg.DATABASE,
+        'user': cfg.PG_USER,
+        'password': cfg.PASSWORD,
+        'port': cfg.PORT,
+    }
